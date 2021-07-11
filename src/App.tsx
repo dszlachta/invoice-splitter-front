254629a5d38ff1fc980f9e228c30a72a0c4df519
Dawid Szlachta <dawids@fr24.com>
2021-07-11 13:39:54 +0200

    Added Blocknative Onboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
+import Onboard from 'bnc-onboard'
+import Web3 from 'web3'
 import logo from './logo.svg';
 import './App.css';
 
+if (!process.env.REACT_APP_NETWORK_ID) {
+  throw new Error('No network id');
+}
+
+let web3;
+
+const onboard = Onboard({
+  dappId: process.env.REACT_APP_BN_API_KET,
+  networkId: parseInt(process.env.REACT_APP_NETWORK_ID, 10),
+  subscriptions: {
+    wallet: wallet => {
+      web3 = new Web3(wallet.provider)
+    }
+  }
+});
+
 function App() {
+  const connect = async () => {
+    const selection = await onboard.walletSelect();
+
+    console.log({ walletSelected: selection });
+
+    const check = await onboard.walletCheck();
+
+    console.log({ check });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <button onClick={connect}>
+            Connect wallet
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
